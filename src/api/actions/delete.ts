96a4c2ee7e2e9aa04820fed9323f40ac75f1bc36
DewaSRY <sdewa6645@gmail.com>
2024-09-03 12:40:09 +0800

    add:create by delete factory

--- src/api/actions/delete.ts
+++ src/api/actions/delete.ts
@@ -1,15 +1,59 @@
 import Collection from "@/api/collection";
 import BrowserBase from "@/browser-base";
+// import isSubSet from "@/utils/isSubset";
 
-export default function fkDelete<T>(parent: T) {
+export default function fkDelete<T, U = unknown>(parent: T) {
   if (parent instanceof BrowserBase) {
     //delete database
   }
   if (parent instanceof Collection) {
     if (parent._filter) {
       // if have filter
+      deleteDataById<U>(parent);
     } else {
       // if there is no filter
+      deleteCollection(parent);
     }
   }
+}
+
+function deleteCollection(collection: Collection<unknown>) {
+  const { _browserBase, collectionName } = collection;
+  _browserBase._deleteCollectionQueue.queue.push(collectionName);
+  if (_browserBase._deleteCollectionQueue.running === false) {
+    _browserBase._deleteCollectionQueue.running = true;
+    _deleteFromQueue(collection);
+  }
+}
+
+function _deleteFromQueue(collection: Collection<unknown>) {
+  const { _browserBase, lf } = collection;
+  if (_browserBase._deleteCollectionQueue.queue.length) {
+    let collectionToDelete = _browserBase._deleteCollectionQueue.queue[0];
+    _browserBase._deleteCollectionQueue.queue.shift();
+    lf.dropInstance({
+      name: _browserBase.dbName,
+      storeName: collectionToDelete,
+    }).then(() => {
+      _deleteFromQueue(collection);
+    });
+  } else {
+    _browserBase._deleteCollectionQueue.running = false;
+  }
+}
+
+function deleteDataById<T>(collection: Collection<T>) {
+  const { lf, _filter } = collection;
+  let docsToSets: string[] = [];
+  new Promise(() => {
+    lf.iterate<T, void>((_value, key) => {
+      if (key === _filter?.id) {
+        docsToSets.push(key);
+      }
+    }).then(() => {
+      docsToSets.forEach((key) => {
+        lf.removeItem(key);
+      });
+    });
+  });
 }
--- src/api/collection/index.ts
+++ src/api/collection/index.ts
@@ -14,6 +14,7 @@ import ftSkip from "./skip";
 
 //action
 import ftAdd from "@/api/actions/add";
+import fkDelete from "@/api/actions/delete";
 
 // import Document from "@/api/selector/doc";
 /**
@@ -24,39 +25,14 @@ export default class Collection<T> {
   public _filter: null | CollectionFilter<T> = null;
 
   constructor(
-    private readonly browserBase: BrowserBase,
-    private readonly collectionName: string
+    public readonly _browserBase: BrowserBase,
+    public readonly collectionName: string
   ) {
     this.lf = localForage.createInstance({
       driver: localForage.INDEXEDDB,
-      name: browserBase.dbName,
+      name: _browserBase.dbName,
       storeName: collectionName,
     });
-  }
-
-  public delete() {
-    const { browserBase, collectionName, deleteFromQueue } = this;
-    browserBase._deleteCollectionQueue.queue.push(collectionName);
-    if (browserBase._deleteCollectionQueue.running === false) {
-      browserBase._deleteCollectionQueue.running = true;
-      deleteFromQueue();
-    }
-  }
-  private deleteFromQueue() {
-    if (this.browserBase._deleteCollectionQueue.queue.length) {
-      let collectionToDelete = this.browserBase._deleteCollectionQueue.queue[0];
-      this.browserBase._deleteCollectionQueue.queue.shift();
-      this.lf
-        .dropInstance({
-          name: this.browserBase.dbName,
-          storeName: collectionToDelete,
-        })
-        .then(() => {
-          this.deleteFromQueue();
-        });
-    } else {
-      this.browserBase._deleteCollectionQueue.running = false;
-    }
   }
 
   //   public doc(docSelectionCriteria: Partial<T>): Document<T> {
@@ -92,5 +68,8 @@ export default class Collection<T> {
   }
   public async get(key: string) {
     return await this.lf.getItem(key).then((doc) => doc);
+  }
+  public delete() {
+    fkDelete<typeof this, T>(this);
   }
 }
